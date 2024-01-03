baf21138a2f358bd0ba61e332c442fb3c966294e
Tasha Miller <popelyshkon25@gmail.com>
2024-01-03 17:33:35 +0200

    Lesson 1. Hello world, Basic syntax

---
+++ Lesson1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Whitespaces
+bool isHappy = true;
+
+/*if (isHappy)
+{
+    Console.WriteLine("I'm happy!");
+}
+else
+{
+    Console.WriteLine("I'm sad!");
+}*/
+
+
+// No Whitespaces
+//bool isHappy = true;
+//if (isHappy) { Console.WriteLine("I'm happy!"); } else { Console.WriteLine("I'm sad!"); }
+
+
+//{
+//    int salary = 500;
+//    Console.WriteLine($"Junior salary is {salary}");
+//}
